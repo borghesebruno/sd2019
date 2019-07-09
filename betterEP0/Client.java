98c2ee7e3d1f0fedd8037b90a5c158154435b3b1
borghesebruno <bb_borghese@hotmail.com>
2019-07-09 17:58:06 -0300

    com consumo de mensagens

--- betterEP0/Client.java
+++ betterEP0/Client.java
@@ -7,22 +7,23 @@ public class Client {
     private static String number;
     private static DatagramSocket socket;
     private static InetAddress address;
-    private static byte[] buffer;
+    private static byte[] buffer = new byte["4".getBytes().length];
     private static DatagramPacket datagram;
 
     public static void main (String args[]) throws IOException, InterruptedException {
-        if(args.length < 2) {
-            System.out.println("Uso correto: java Cliente <1|2> <seq|fora|falta|dupla>");
+        if(args.length < 1) {
+            System.out.println("Uso correto: java Cliente <seq|fora|falta|dupla>");
             return;
         }
 
-        number = args[0];
-        System.out.println("Iniciando Cliente " + number);
-
-        socket = new DatagramSocket();
         address = InetAddress.getByName("localhost");
+        socket = new DatagramSocket();
+
+        String clientName = InetAddress.getByName("localhost").getHostAddress() +':'+ socket.getPort();
+        System.out.println("Cliente iniciado no endereço: " + clientName);
+
         
-        switch(args[1]) {
+        switch(args[0]) {
             case "seq":
                 System.out.println("Enviando mensagens ordenadas:");
                 sendOrdered();
@@ -43,13 +44,13 @@ public class Client {
                 break;
         }
 
-        System.out.println("Encerrando Cliente " + number);
+        System.out.println("Encerrando cliente do endereço " + clientName);
         socket.close();
     }
 
     private static void sendOrdered () throws IOException, InterruptedException {
         for(int i = 1; i < 5; i++) {
-            buffer = (number + '-' + i).getBytes();
+            buffer = (""+i).getBytes();
             System.out.println("--- enviando: " + i);
             datagram = new DatagramPacket(buffer, buffer.length, address, PORT);
             socket.send(datagram);
@@ -60,7 +61,7 @@ public class Client {
     private static void sendUnordered () throws IOException, InterruptedException {
         int[] nums = {3,1,4,2};
         for(int i = 0; i < 4; i++) {
-            buffer = (number + '-' + nums[i]).getBytes();
+            buffer = (""+nums[i]).getBytes();
             System.out.println("--- enviando: " + nums[i]);
             datagram = new DatagramPacket(buffer, buffer.length, address, PORT);
             socket.send(datagram);
@@ -71,7 +72,7 @@ public class Client {
     private static void sendMissing () throws IOException, InterruptedException {
         for(int i = 1; i < 5; i++) {
             if(i != 2 && i != 3) {
-                buffer = (number + '-' + i).getBytes();
+                buffer = (""+i).getBytes();
                 System.out.println("--- enviando: " + i);
                 datagram = new DatagramPacket(buffer, buffer.length, address, PORT);
                 socket.send(datagram);
@@ -83,7 +84,7 @@ public class Client {
     private static void sendDuplicated () throws IOException, InterruptedException {
         int[] nums = {1,1,2,3,2,3,4,4};
         for(int i = 0; i < 8; i++) {
-            buffer = (number + '-' + nums[i]).getBytes();
+            buffer = (""+nums[i]).getBytes();
             System.out.println("--- enviando: " + nums[i]);
             datagram = new DatagramPacket(buffer, buffer.length, address, PORT);
             socket.send(datagram);
--- betterEP0/Server.java
+++ betterEP0/Server.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.util.HashMap;
 import java.io.*;
 
 public class Server {
@@ -6,65 +7,73 @@ public class Server {
         System.out.println("Iniciando Servidor");
 
         DatagramSocket serverSocket = new DatagramSocket(1234);
-
-        ServerThread[] threads = { new ServerThread("1"), new ServerThread("2") };
-        threads[0].start();
-        threads[1].start();
-        boolean[] completed = { false, false };
-
         byte[] receiveData;
-        boolean goOn = true;
-        while(goOn)
-        {
+
+        HashMap<String,ServerThread> map = new HashMap<String,ServerThread>();
+
+        do {
             receiveData = new byte[128];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             
-            String[] received = new String(receivePacket.getData()).split("-");
-            int fromClient = Integer.parseInt(received[0]);
-            String msg = received[1];
-            System.out.println("Mensagem recebida do cliente " + fromClient + ": " + msg);
-            
-            completed[fromClient - 1] = threads[fromClient - 1].receive(msg);
-            if(completed[0] && completed[1]) {
-                goOn = false;
+            String received = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            String fromClient = receivePacket.getAddress().getHostAddress() +':'+ receivePacket.getPort();
+            System.out.println("Mensagem recebida do cliente " + fromClient + ": " + received);
+
+            if(map.containsKey(fromClient)) {
+                ServerThread thread = map.get(fromClient);
+                if(thread.isAlive()) thread.receive(received);
+            } else {
+                ServerThread thread = new ServerThread(fromClient);
+                thread.start();
+                map.put(fromClient, thread);
+                thread.receive(received);
             }
-        }
+
+            System.out.println(map.size());
+        } while(map.size() < 3);
 
         serverSocket.close();
     }
 }
 
 class ServerThread extends Thread {
-    private String threadNumber;
+    private String fromClient;
     private boolean[] msgs = {false, false, false, false};
-    private boolean complete = false;
 
-    ServerThread(String number) {
-        threadNumber = number;
+    ServerThread(String fromClient) {
+        this.fromClient = fromClient;
     }
     
     public void run() {
         try {
-            while(!complete) {;}
+            Thread.sleep(10000);
+            System.out.println("Consumindo mensagens recebidas do endereço " + fromClient);
+            System.out.println("Mensagem 1: " + (msgs[0] ? "OK" : "Não recebida"));
+            System.out.println("Mensagem 2: " + (msgs[1] ? "OK" : "Não recebida"));
+            System.out.println("Mensagem 3: " + (msgs[2] ? "OK" : "Não recebida"));
+            System.out.println("Mensagem 4: " + (msgs[3] ? "OK" : "Não recebida"));
         }
         catch (Exception e) {
 
         }
     }
 
-    public boolean receive(String msg) {
+    public void receive(String msg) {
         switch(msg) {
-            case "1": msgs[0] = true;
-            case "2": msgs[1] = true;
-            case "3": msgs[2] = true;
-            case "4": msgs[3] = true;
+            case "1":
+                msgs[0] = true;
+                break;
+            case "2":
+                msgs[1] = true;
+                break;
+            case "3":
+                msgs[2] = true;
+                break;
+            case "4":
+                msgs[3] = true;
+                break;
             default: break;
         }
-        complete = msgs[0] && msgs[1] && msgs[2] && msgs[3];
-        if(complete) {
-            System.out.println("Todas as mensagens do cliente " + threadNumber + " foram recebidas.");
-        }
-        return complete;
     }
 }
