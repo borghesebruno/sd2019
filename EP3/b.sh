db2ee977e957735af27919f0fda0621ae8f8aa3f
borghesebruno <bb_borghese@hotmail.com>
2019-08-20 22:16:12 -0300

    EP3

---
+++ EP3/b.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf files
+mkdir files
+cd files
+
+no_of_files=3;
+counter=1;
+while [ $counter -le $no_of_files ];
+do
+    echo file $counter;
+    rand=`shuf -i 10-100 -n 1`;
+    touch random-$counter-$rand.txt;
+    counter=`expr $counter + 1`;
+done
+
+cd ..
+
+#javac Peer.java
