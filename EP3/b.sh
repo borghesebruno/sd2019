214303b29ea733d5de69aa3c83fb6d9b263e9884
borghesebruno <bb_borghese@hotmail.com>
2019-08-21 00:28:13 -0300

    EP3 estrutura

--- EP3/b.sh
+++ EP3/b.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-rm -rf files
-mkdir files
-cd files
-
-no_of_files=3;
-counter=1;
-while [ $counter -le $no_of_files ];
-do
-    echo file $counter;
-    rand=`shuf -i 10-100 -n 1`;
-    touch random-$counter-$rand.txt;
-    counter=`expr $counter + 1`;
-done
-
-cd ..
+javac -cp .:jsoup-1.12.1.jar *.java
